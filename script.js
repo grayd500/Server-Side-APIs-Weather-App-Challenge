16595063b942f04cd4c36784d6b1eef571903421
David <grayd500@gmail.com>
2023-09-10 17:06:04 -0500

    fix JS

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Define a constant for the API key
 const apiKey = "17c217eb87e1e62fc400a04808b0be98";
 
-// Wait until the HTML document is loaded
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   const searchBtn = document.getElementById("searchBtn");
+  
+  loadSearchHistory(); // Load the previously searched cities
 
-  // When you click the search button, this code will run
-  searchBtn.addEventListener("click", function() { 
+  searchBtn.addEventListener("click", function() {
     const cityName = document.getElementById("cityInput").value;
-
-    // This is like asking a magic weather website for the 5-day forecast
+    saveSearchHistory(cityName); // Save the searched city name
+    loadSearchHistory(); // Reload the search history buttons
+    
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`)
       .then(response => response.json())
       .then(data => {
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const lat = data.city.coord.lat;
         const lon = data.city.coord.lon;
 
-        // Now we ask the magic weather website for the weather right now
         return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
       })
       .then(response => response.json())
@@ -27,23 +26,47 @@ document.addEventListener("DOMContentLoaded", function() {
       })
       .catch(error => console.error("Something went wrong:", error));
   });
-
-  const presetButtons = document.querySelectorAll(".preset-city-btn");
-  
-  presetButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
-      const presetCity = this.getAttribute("data-city");
-      document.getElementById("cityInput").value = presetCity;
-      searchBtn.click();
-    });
-  });
 });
 
+function loadSearchHistory() {
+  const historyContainer = document.getElementById('searchBtn').parentNode;
+  const oldHistory = document.querySelectorAll(".history-button");
+  
+  // Remove old history buttons
+  oldHistory.forEach(button => button.remove());
+  
+  const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  
+  searchHistory.forEach(city => {
+    const historyButton = document.createElement("button");
+    historyButton.textContent = city;
+    historyButton.className = "btn btn-secondary btn-lg btn-block my-1 history-button";
+    
+    historyButton.addEventListener("click", function() {
+      document.getElementById("cityInput").value = city;
+      document.getElementById("searchBtn").click();
+    });
+    
+    historyContainer.appendChild(historyButton);
+  });
+}
+
+function saveSearchHistory(cityName) {
+  const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  
+  if (!searchHistory.includes(cityName)) {
+    searchHistory.push(cityName);
+  }
+  
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+}
+
+// 5-Day Forecast
 function updateFiveDayForecast(data) {
   for (let i = 1; i <= 5; i++) {
     const dayData = data.list[i];
     const formattedDate = new Date(dayData.dt_txt).toLocaleDateString();
-    const weatherEmoji = `<img src="http://openweathermap.org/img/wn/${dayData.weather[0].icon}.png">`;
+    const weatherEmoji = `<img src="https://openweathermap.org/img/wn/${dayData.weather[0].icon}.png">`;
     const temperature = dayData.main.temp;
     const windSpeed = dayData.wind.speed;
     const humidity = dayData.main.humidity;
@@ -57,17 +80,20 @@ function updateFiveDayForecast(data) {
     `;
   }
 }
-
+// Current Weather
 function updateCurrentWeather(currentData, cityName) {
-  const currentTemperature = currentData.main.temp;
-  const currentWindSpeed = currentData.wind.speed;
-  const currentHumidity = currentData.main.humidity;
+  try {
+    const currentTemperature = currentData.main.temp;
+    const currentWindSpeed = currentData.wind.speed;
+    const currentHumidity = currentData.main.humidity;
 
-  document.getElementById("cityData").innerHTML = `
-    ${cityName}<br>
-    Temp: ${currentTemperature} F<br>
-    Windspeed: ${currentWindSpeed} MPH<br>
-    Humidity: ${currentHumidity}%<br>
-    Date: ${new Date().toLocaleDateString()}<br>
-  `;
+    document.getElementById("cityData").innerHTML = `
+      ${cityName} (${new Date().toLocaleDateString()})</br>
+      Temp: ${currentTemperature} F<br>
+      Windspeed: ${currentWindSpeed} MPH<br>
+      Humidity: ${currentHumidity}%<br>
+    `;
+  } catch (error) {
+    console.error("Error in updateCurrentWeather:", error);
+  }
 }
