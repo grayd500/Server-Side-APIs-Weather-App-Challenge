702def1732e7200f62c3beeb5be84d8957e755ac
David <grayd500@gmail.com>
2023-09-09 19:26:55 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// First, we save the city name and API key in variables. Like putting your lunch in a lunchbox!
+const cityName = "CITY_NAME";
+const apiKey = "17c217eb87e1e62fc400a04808b0be98";
+
+// Then we ask the OpenWeatherMap for weather info for that city.
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+  .then(response => response.json())  // Once we get a reply, we read the info they sent back.
+  .then(data => {
+    console.log(data);  // We can check the whole info in the console.
+
+    // Here's how to find the latitude and longitude in the info.
+    const lat = data.city.coord.lat;
+    const lon = data.city.coord.lon;
+
+    // Now you can use these for anything else you want!
+  })
+  .catch(error => console.error("Something went wrong:", error));  // If something goes wrong, we'll see an error message.
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Find the button element on the page
+    const searchBtn = document.getElementById("searchBtn");
+  
+    // Listen for a click on the button
+    searchBtn.addEventListener("click", function() {
+      // Find out what city name was typed into the input field
+      const cityName = document.getElementById("cityInput").value;
+      const apiKey = "17c217eb87e1e62fc400a04808b0be98";
+  
+      // Ask OpenWeatherMap for the weather in that city
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`)
+
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+  
+          // Grabbing the weather details
+          const cityName = data.city.name;
+          const temperature = data.list[0].main.temp;  // This is in Farenheight
+          const windSpeed = data.list[0].wind.speed;  // This is in meters per second
+          const humidity = data.list[0].main.humidity; // This is a percentage
+  
+          // Updating the #cityData div
+          document.getElementById("cityData").innerHTML = `
+            City name: ${cityName} <br>
+            Temp: ${temperature} K<br>
+            Wind: ${windSpeed} m/s<br>
+            Humidity: ${humidity}%
+          `;
+        })
+        .catch(error => console.error("Something went wrong:", error));
+    });
+  });
+  
